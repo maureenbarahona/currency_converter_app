bbf59ffa01fb25cd8a17ef0f7f110ca20054dfb4
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-25 14:22:00 -0600

    deloy

---
+++ build/precache-manifest.94233d9a7ca9ce840801dac18d0a4704.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816d552fa5e90a4525ee27720d19dd25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a8176b583f0d034721a",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c79d68268befd0f6dc73",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a8176b583f0d034721a",
+    "url": "/static/js/2.7890fc5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7890fc5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79d68268befd0f6dc73",
+    "url": "/static/js/main.2e47d88a.chunk.js"
+  },
+  {
+    "revision": "2b34daf86348e0cbcc46",
+    "url": "/static/js/runtime-main.45ff9079.js"
+  }
+]);
